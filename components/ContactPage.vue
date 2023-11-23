c79502d2098af248da6c9409712d3aa7240caead
Chopper <matheus.ricieri@hotmail.com>
2023-11-23 16:45:37 -0300

    Add ContactPage code

--- components/ContactPage.vue
+++ components/ContactPage.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+    <p class="text-center">Entre em contato</p>
+    <div class="flex justify-center gap-x-4">
+        <div v-for="(contact, contactKey) in contacts" :key="contactKey">
+            <i :class="`devicon-${contact}`" class="text-5xl"></i>
+        </div>
+    </div>
+</template>
+<script setup>
+const contacts = ["github-original", "linkedin-plain"];
+</script>
