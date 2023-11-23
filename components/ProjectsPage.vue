7fb2770555ab9a1a8aa77ce3264d9658d31e726b
Chopper <matheus.ricieri@hotmail.com>
2023-11-23 16:45:47 -0300

    Add ProjectsPage code

--- components/ProjectsPage.vue
+++ components/ProjectsPage.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+    <div class="flex flex-col">
+        <p class="text-center">Alguns dos meus projetos</p>
+        <div class="flex flex-wrap gap-2 justify-center">
+            <div class="w-40 h-20 bg-white"></div>
+            <div class="w-40 h-20 bg-white"></div>
+            <div class="w-40 h-20 bg-white"></div>
+            <div class="w-40 h-20 bg-white"></div>
+        </div>
+    </div>
+</template>
