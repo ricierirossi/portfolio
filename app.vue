6a939a06ffd79513b2fdd7268b9b1373f85f2847
Chopper <matheus.ricieri@hotmail.com>
2023-11-23 16:45:53 -0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,14 +1,15 @@
 <template>
-    <div
-        class="bg-charcoal h-screen text-misty font-julius flex flex-col justify-center"
-    >
-        <!-- <HomePage /> -->
-        <AboutPage />
-        <!-- <ProjectsPage /> -->
-        <!-- <ContactPage /> -->
-
+    <div class="bg-charcoal text-misty font-julius h-full">
+        <div class="flex flex-col gap-y-24 px-6 py-7">
+            <HomePage />
+            <AboutPage />
+            <ProjectsPage />
+            <ContactPage />
+        </div>
         <NavBar
-            class="bg-charcoal absolute bottom-0 w-full max-h-14 py-3 sm:text-2xl lg:absolute lg:top-0 2xl:text-4xl"
+            class="bg-charcoal flex justify-around sticky bottom-0 w-full"
         />
     </div>
 </template>
+
+<!-- TODO continuar responsividade no AboutPage -->
