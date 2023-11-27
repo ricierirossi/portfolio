c7ae4d64109ec6414b0450d3e11c3b591d08f0db
Chopper <matheus.ricieri@hotmail.com>
2023-11-27 18:01:46 -0300

    Rename sections

--- app.vue
+++ app.vue
@@ -1,15 +1,11 @@
 <template>
-    <div class="bg-charcoal text-misty font-julius h-full">
-        <div class="flex flex-col gap-y-24 px-6 py-7">
-            <HomePage />
-            <AboutPage />
-            <ProjectsPage />
-            <ContactPage />
-        </div>
-        <NavBar
-            class="bg-charcoal flex justify-around sticky bottom-0 w-full"
-        />
+  <div class="bg-charcoal text-misty font-julius h-full">
+    <div class="grid grid-rows-layout">
+      <HomeSection />
+      <AboutSection />
+      <ProjectsSection />
+      <ContactSection />
     </div>
+    <NavBar />
+  </div>
 </template>
-
-<!-- TODO continuar responsividade no AboutPage -->
