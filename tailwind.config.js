fc159a30d3a65a05eec4764bafabfa95f8272a0e
Chopper <matheus.ricieri@hotmail.com>
2023-11-27 18:02:57 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./components/**/*.{js,vue,ts}",
-        "./layouts/**/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
-        "./app.vue",
-        "./error.vue",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                charcoal: "#242424",
-                misty: "#C2BEBE",
-            },
-            fontFamily: {
-                julius: ["Julius Sans One"],
-            },
-        },
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: "#242424",
+        misty: "#C2BEBE",
+      },
+      fontFamily: {
+        julius: ["Julius Sans One"],
+      },
+      gridTemplateRows: {
+        layout: "repeat(4, minmax(100vh, 1fr))",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
