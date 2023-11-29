2b5d9b77679742c27a8443e5f90065098d6348dd
Chopper <matheus.ricieri@hotmail.com>
2023-11-29 12:36:03 -0300

    Add id and contact links

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="grid items-center">
+  <div id="3" class="grid items-center">
     <p class="text-center text-4xl sm:text-5xl md:text-6xl">Entre em contato</p>
     <div
       class="flex flex-col self-start items-center gap-y-32 lg:flex-row lg:justify-center lg:gap-x-32"
     >
-      <div v-for="(contact, contactKey) in contacts" :key="contactKey">
-        <i
-          :class="`devicon-${contact}`"
+      <a href="https://github.com/ricierirossi" target="_blank"
+        ><i
+          :class="`devicon-github-original`"
           class="text-9xl hover:cursor-pointer"
-        ></i>
-      </div>
+        ></i
+      ></a>
+      <a
+        href="https://www.linkedin.com/in/matheus-ricieri-rossi-da-silva-17768a1b8/"
+        target="_blank"
+        ><i
+          :class="`devicon-linkedin-plain`"
+          class="text-9xl hover:cursor-pointer"
+        ></i
+      ></a>
     </div>
   </div>
 </template>
-<script setup>
-const contacts = ["github-original", "linkedin-plain"];
-</script>
