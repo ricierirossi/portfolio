e1c69f745dfbb2e1f82f59c01d012b1778336e92
Chopper <matheus.ricieri@hotmail.com>
2023-11-29 14:13:41 -0300

    Remove link tag

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,9 +1,4 @@
 <template>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-  />
-
   <div
     id="1"
     class="flex flex-col justify-center items-center gap-y-7 lg:grid lg:grid-cols-2 grid-rows-3 lg:py-11 lg:gap-0"
