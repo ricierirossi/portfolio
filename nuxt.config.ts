13e16874f86eb2ecc19f41702aaf8052bf874083
Chopper <matheus.ricieri@hotmail.com>
2023-11-29 14:13:54 -0300

    Add head, meta and link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,19 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      title: 'Matheus Ricieri',
+      meta: [
+        {
+          name:'description',
+          content: 'Explore o universo do desenvolvimento front-end por meio do meu portfólio! Sou Matheus Ricieri, um apaixonado desenvolvedor front-end, e convido você a mergulhar nos meus projetos que destacam habilidades sólidas em HTML, CSS e JavaScript. Entre em contato diretamente comigo para oportunidades de colaboração e descubra mais sobre minha jornada e visão no desenvolvimento web. Vamos criar juntos a próxima experiência digital extraordinária!'
+        }
+      ],
+      link: [{
+        rel:"stylesheet",
+        href:"https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+      }]
+    }
+  }
 })
