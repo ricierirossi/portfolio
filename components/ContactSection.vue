5492bcea4237a82e8cbf882856b529d76eb62d9b
Chopper <matheus.ricieri@hotmail.com>
2023-11-30 12:26:16 -0300

    Add icons names and alt

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -2,20 +2,25 @@
   <div id="3" class="grid items-center">
     <p class="text-center text-4xl sm:text-5xl md:text-6xl">Entre em contato</p>
     <div
-      class="flex flex-col self-start items-center gap-y-32 lg:flex-row lg:justify-center lg:gap-x-32"
+      class="flex self-start justify-center gap-x-20 text-center lg:gap-x-32"
     >
-      <a href="https://github.com/ricierirossi" target="_blank"
-        ><i
+      <a href="https://github.com/ricierirossi" target="_blank">
+        <p class="text-xl">Github</p>
+        <i
           :class="`devicon-github-original`"
-          class="text-9xl hover:cursor-pointer"
+          class="text-6xl hover:cursor-pointer sm:text-8xl"
+          alt="github"
         ></i
       ></a>
       <a
         href="https://www.linkedin.com/in/matheus-ricieri-rossi-da-silva-17768a1b8/"
         target="_blank"
-        ><i
+      >
+        <p class="text-xl">LinkedIn</p>
+        <i
           :class="`devicon-linkedin-plain`"
-          class="text-9xl hover:cursor-pointer"
+          class="text-6xl hover:cursor-pointer sm:text-8xl"
+          alt="linkedin"
         ></i
       ></a>
     </div>
