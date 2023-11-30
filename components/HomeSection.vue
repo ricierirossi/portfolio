a6c48654b04747be46be2f2cb17a7c0fa172bb56
Chopper <matheus.ricieri@hotmail.com>
2023-11-30 12:25:17 -0300

    Add more info

--- components/HomeSection.vue
+++ components/HomeSection.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="0" class="flex flex-col justify-center items-center gap-y-24 pt-20">
-    <div class="text-center text-6xl sm:text-7xl md:text-8xl lg:9xl">
-      <p>Matheus</p>
-      <p>Ricieri</p>
+  <section id="0" class="flex flex-col justify-center text-center px-5">
+    <div class="flex flex-col gap-y-10 pb-10">
+      <h1>
+        <p class="text-3xl sm:text-4xl lg:text-5xl">
+          Olá, sou o <br /><b>Matheus Ricieri</b>
+        </p>
+      </h1>
+      <div class="text-base sm:text-lg lg:text-xl">
+        <h2>
+          <p>Desenvolvedor Front-end</p>
+          <p>Apaixonado por criar</p>
+        </h2>
+      </div>
+      <h3
+        class="text-base sm:text-lg lg:text-xl text-center self-center max-w-xs"
+      >
+        <p>
+          Convido você a mergulhar nos meus projetos e descubrir mais sobre
+          minha jornada e visão no desenvolvimento web.
+        </p>
+      </h3>
+      <p class="text-2xl">&lt;/&gt;</p>
     </div>
-    <p
-      class="text-center text-3xl sm:text-4xl md:text-5xl lg:w-full lg:text-end lg:pr-10 lg:text-6xl"
-    >
-      &lt;/&gt;
-    </p>
-  </div>
+  </section>
 </template>
