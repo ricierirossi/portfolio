8008669b6ba1a5ec6494d12eb6b4563ec9118d31
Chopper <matheus.ricieri@hotmail.com>
2023-12-05 16:58:32 -0300

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,16 @@ export default defineNuxtConfig({
       {
         rel:"stylesheet",
         href:"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      },
+      {
+        rel:"icon",
+        href:"/favicon-32x32.png",
+        sizes:'32x32'
+      },
+      {
+        rel:"icon",
+        href:"/favicon-16x16.png",
+        sizes:'16x16'
       }
       ]
     }
