2101c1bebc395505335d923e1b2d6adbd8d8017f
Chopper <matheus.ricieri@hotmail.com>
2023-12-05 13:35:42 -0300

    Add light mode component

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="bg-charcoal text-misty font-julius h-full">
+  <div class="bg-charcoal light:bg-white text-misty font-julius h-full">
     <div class="grid grid-rows-layout">
+      <LightModeButton />
       <HomeSection />
       <AboutSection />
       <ProjectsSection />
