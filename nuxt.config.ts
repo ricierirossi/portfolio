63ab9c2e00ebb584435d740d83c3061acb10424f
Chopper <matheus.ricieri@hotmail.com>
2023-12-06 14:49:13 -0300

    Remove unused links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,20 +26,6 @@ export default defineNuxtConfig({
         rel:"stylesheet",
         href:"https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
       },
-      {
-        rel:"stylesheet",
-        href:"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-      },
-      {
-        rel:"icon",
-        href:"/favicon-32x32.png",
-        sizes:'32x32'
-      },
-      {
-        rel:"icon",
-        href:"/favicon-16x16.png",
-        sizes:'16x16'
-      }
       ]
     }
   }
