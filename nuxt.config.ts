81a206d6027e8fbc686bcdd0f8762b44a57c33a4
Chopper <matheus.ricieri@hotmail.com>
2023-12-06 15:54:03 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
       link: [
       {
         rel:"stylesheet",
-        href:"https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        href:"https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css",
       },
       ]
     }
